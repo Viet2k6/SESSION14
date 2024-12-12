f7187729f7030c9415a028e48b023291113a7da4
Đăng Việt <trandangviet00@gmail.com>
2024-12-12 11:30:49 +0700

    Add files via upload

--- ss14-b1.c
+++ ss14-b1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-    char string[100];
-    printf("Moi nhap chuoi: ");
-    fgets(string, sizeof(string), stdin);
-    size_t size = strlen(string);
-    printf("Chuoi vua nhap: %s", string);
-    printf("Do dai chuoi: %zu\n", size);
 
-    return 0;
+int main(){
+	char string[100];
+	printf("Moi nhap chuoi: ");
+	fgets(string,sizeof string,stdin);
+	string[strcspn(string,"\n")]='\0';
+	printf("Chuoi %s va do dai chuoi %d",string,strlen(string));
+	
+	return 0;
 }
